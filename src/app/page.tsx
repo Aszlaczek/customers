a0e762404ea545824f9ee57ebf7880346c4c73c6
Aszlaczek <awzorek23@wp.pl>
2024-09-20 17:40:58 +0200

    Cosmetic change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "@/css/page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hello into my users interface</h1>
+      <h1 className={styles.h1}>Hello into my users interface</h1>
     </div>
   );
 }
